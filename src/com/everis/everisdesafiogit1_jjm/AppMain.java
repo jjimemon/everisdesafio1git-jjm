0ec664a4a0ea360630d90caf6af4960ecaeb654c
jjimemon <jjimemon@everis.com>
2021-03-25 10:16:32 +0100

    Version 1 desafio GIT

---
+++ src/com/everis/everisdesafiogit1_jjm/AppMain.java
@@ -0,0 +1,25 @@
+package com.everis.everisdesafiogit1_jjm;
+
+/**
+ * 
+ * Formacion Everis - Desafio 1 Git
+ * 
+ * @author jjimemon
+ *
+ */
+
+public class AppMain {
+	
+	/**
+	 * Clase Main de la aplicación.
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola mundo");
+		
+	}
+
+}
